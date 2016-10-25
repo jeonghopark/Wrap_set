925908f19fa676494b6bf87bd3a9593da20d8fbd
jeonghopark <jeonghopark@jeonghopark.de>
2016-10-25 12:13:09 +0200

    First setup

---
+++ src/ofApp.cpp
@@ -0,0 +1,82 @@
+#include "ofApp.h"
+#include "stdio.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){	 
+	img.load("test_background.png");
+    
+	for (int i =0; i<2; i++){
+        
+        warpers[i] = *new ofxGLWarper();
+        warpers[i].setup((i%2)*img.getWidth(),floor(float(i)/2)*img.getHeight(),img.getWidth(), img.getHeight());
+       //warpers.back().activate();
+    }
+    ofSetVerticalSync(true);
+	
+	activeWarper =-1;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){	
+	ofBackground(20, 20, 20);
+	
+}
+//--------------------------------------------------------------
+void ofApp::draw(){
+    for (int i =0; i<2; i++){
+        warpers[i].begin();	
+        warpers[i].draw(); 
+        img.draw((i%2)*img.getWidth(),floor(float(i)/2)*img.getHeight());
+        warpers[i].draw();
+        warpers[i].end();
+	}
+    ofDrawBitmapString("Press keys 1 to 4 to toggle each warper", 20, 50);
+}
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+  bool bDeactivateOthers = false;
+    switch (key) {
+        case '1':
+            activeWarper=0;
+            warpers[0].toogleActive();
+            bDeactivateOthers =true;
+            break;
+            //*
+        case '2':
+            activeWarper=1;
+            warpers[1].toogleActive();
+            bDeactivateOthers =true;
+            break;
+        case '3':
+            activeWarper=2;        
+            bDeactivateOthers =true;
+            warpers[2].toogleActive();
+            break;
+        case '4':
+            activeWarper=3;        
+            bDeactivateOthers =true;
+            warpers[3].toogleActive();
+            break; 
+            //*/
+            default:
+            break;
+    }
+    
+    if (bDeactivateOthers) {
+        for (int i =0; i<2; i++){
+            if (i!=activeWarper) {
+                warpers[i].deactivate();
+            }
+        }
+    }
+}
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){}
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){}
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){}
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){}
+//--------------------------------------------------------------
+void ofApp::mouseReleased(){}
---
+++ src/ofApp.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGLWarper.h"
+
+class ofApp : public ofSimpleApp{
+	
+	public:
+		
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased();
+
+		ofxGLWarper warpers[2];
+	
+		ofImage img;
+    int activeWarper;
+};
+
+
